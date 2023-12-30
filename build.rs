65ab90d5bbb8934580268fe302f4dfda1d3b0317
Twirrim <paulgraydon@hotmail.com>
2023-12-30 16:02:16 +0000

    Use a long chain of ifs

--- build.rs
+++ build.rs
@@ -10,23 +10,25 @@ fn main() {
     let mut nasty_rs = File::create(&dest_path).expect("Cannot write to file");
     // Header
     writeln!(&mut nasty_rs, "pub fn is_even(number: u32) -> bool {{").unwrap();
-    writeln!(&mut nasty_rs, "   match number  {{").unwrap();
     // Loop!
     for i in 0..=(u32::MAX / 1000) {
         if i % 2 == 0 {
-            writeln!(&mut nasty_rs, "  {i}  =>  true,    ").unwrap();
+            writeln!(
+                &mut nasty_rs,
+                "    if number == {i} {{\n        return true; \n    }}\n"
+            )
+            .unwrap();
         } else {
-            writeln!(&mut nasty_rs, "  {i}  =>  false,    ").unwrap();
+            writeln!(
+                &mut nasty_rs,
+                "    if number == {i} {{\n        return false; \n    }}\n"
+            )
+            .unwrap();
         }
     }
     // Footer
-    writeln!(
-        &mut nasty_rs,
-        "  _  =>  panic!(\"Unsupported number\"),    "
-    )
-    .unwrap();
-    writeln!(&mut nasty_rs, "   }}").unwrap();
-    writeln!(&mut nasty_rs, " }}").unwrap();
+    writeln!(&mut nasty_rs, "    return false;").unwrap();
+    writeln!(&mut nasty_rs, "}}").unwrap();
 
     println!("cargo:rerun-if-changed=build.rs");
 }
